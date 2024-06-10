9e76b4819f0521d9f1b8561b14c335fc5d6ab7ac
Ricardo <ricardoleonardo097@gmail.com>
2024-06-10 19:51:28 +0200

    an input is added

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const mostrar = document.getElementById("mostrar-gif");
 
 const buscarGif = async () => {
   let busqueda = document.getElementById("txt-buscar").value;
+  let cant = document.getElementById("txt-cantidad").value;
   const apiKey = "HgWIlXViCM2fS8qqXgF68g1WTjaad0Q2";
-  const urlGif = `https://api.giphy.com/v1/gifs/search?api_key=HgWIlXViCM2fS8qqXgF68g1WTjaad0Q2&q=${busqueda}&limit=6&offset=0&rating=g&lang=es&bundle=messaging_non_clips`;
+  const urlGif = `https://api.giphy.com/v1/gifs/search?api_key=HgWIlXViCM2fS8qqXgF68g1WTjaad0Q2&q=${busqueda}&limit=${cant}&offset=0&rating=g&lang=es&bundle=messaging_non_clips`;
   const requestData = await fetch(urlGif);
   const data = await requestData.json();
 
@@ -19,7 +20,17 @@ const buscarGif = async () => {
   });
 };
 
+const validarCantidad = () => {
+  const cantIngresada = document.getElementById("txt-cantidad");
+  let cant = cantIngresada.value;
+  if (cant < 3) {
+    cantIngresada.value = 3;
+  }
+};
+
 boton.addEventListener("click", async () => {
+  validarCantidad();
   await buscarGif();
   document.getElementById("txt-buscar").value = "";
+  document.getElementById("txt-cantidad").value = "";
 });
