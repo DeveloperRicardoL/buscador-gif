e5925282a8af342dfc74782e183c928f30143f09
Ricardo <ricardoleonardo097@gmail.com>
2024-06-10 13:22:53 +0200

    added functionality

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 import "./style.css";
 
 const boton = document.querySelector("#btn-buscar");
+const mostrar = document.getElementById("mostrar-gif");
 
-const buscarGif = () => {
-  let resp = document.getElementById("txt-buscar").ariaValueMax.toUpperCase;
-  return resp;
+const buscarGif = async () => {
+  let busqueda = document.getElementById("txt-buscar").value;
+  const apiKey = "HgWIlXViCM2fS8qqXgF68g1WTjaad0Q2";
+  const urlGif = `https://api.giphy.com/v1/gifs/search?api_key=HgWIlXViCM2fS8qqXgF68g1WTjaad0Q2&q=${busqueda}&limit=6&offset=0&rating=g&lang=es&bundle=messaging_non_clips`;
+  const requestData = await fetch(urlGif);
+  const data = await requestData.json();
+
+  mostrar.innerHTML = "";
+
+  data.data.forEach((gif) => {
+    const img = document.createElement("img");
+    img.src = gif.images.fixed_height.url;
+    mostrar.appendChild(img);
+  });
 };
 
-boton.addEventListener("click", () => {});
+boton.addEventListener("click", async () => {
+  await buscarGif();
+  document.getElementById("txt-buscar").value = "";
+});
